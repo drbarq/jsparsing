07fe28628e5d65a6d7f58891d698f7437da1c911
Joe Tustin <J.Tustin@gmail.com>
2023-07-13 16:02:52 -0600

    adding in new files for flavors

--- serviceCatalog/csvDic/index.js
+++ serviceCatalog/csvDic/index.js
@@ -7,13 +7,10 @@ function csvToDictionary(csvFilePath, callback) {
   fs.createReadStream(csvFilePath)
     .pipe(csv())
     .on("data", (data) => {
-      const key = `row_${rowIndex}`; // Generate a unique identifier based on row number
+      const key = `row_${rowIndex}`;
+      // Generate a unique identifier based on row number
       dictionary[key] = data;
       rowIndex++;
-      for (var key2 in data) {
-        // value = data[key2];
-        dictionary[key] = data;
-      }
     })
     .on("end", () => {
       callback(null, dictionary);
@@ -23,50 +20,13 @@ function csvToDictionary(csvFilePath, callback) {
     });
 }
 
-function csvToArray(csvFilePath, callback) {
-  var migrationArray = [];
-  //   let rowIndex = 1;
-  fs.createReadStream(csvFilePath)
-    .pipe(csv())
-    .on("data", (data) => {
-      //   const key = `row_${rowIndex}`; // Generate a unique identifier based on row number
-
-      //   dictionary[key] = data;
-      //   rowIndex++;
-      migrationArray.push(data);
-      //   for (var key2 in data) {
-      //     // value = data[key2];
-      //     migrationArray.push(data);
-      //     // dictionary[key] = data;
-      //   }
-    })
-    .on("end", () => {
-      callback(null, migrationArray);
-    })
-    .on("error", (error) => {
-      callback(error);
-    });
-}
-
 // Usage:
-const csvFilePath = "./migration2.csv";
+const csvFilePath = "./migration.csv";
 
-// csvToDictionary(csvFilePath, (error, dictionary) => {
-//   if (error) {
-//     console.error("Error:", error);
-//     return;
-//   }
-
-//   console.log("Dictionary:", dictionary);
-//   console.log("Dictionary:", dictionary);
-//   //   return;
-// });
-csvToArray(csvFilePath, (error, array) => {
+csvToDictionary(csvFilePath, (error, dictionary) => {
   if (error) {
     console.error("Error:", error);
     return;
   }
-
-  console.log("array:", array[6]);
-  //   return;
+  console.log("Dictionary:", dictionary);
 });
---
+++ serviceCatalog/csvDic/migrationArray.js
@@ -0,0 +1,27 @@
+const csv = require("csv-parser");
+const fs = require("fs");
+
+function csvToArray(csvFilePath, callback) {
+  var migrationArray = [];
+  fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on("data", (data) => {
+      migrationArray.push(data);
+    })
+    .on("end", () => {
+      callback(null, migrationArray);
+    })
+    .on("error", (error) => {
+      callback(error);
+    });
+}
+
+const csvFilePath = "./migration2.csv";
+
+csvToArray(csvFilePath, (error, array) => {
+  if (error) {
+    console.error("Error:", error);
+    return;
+  }
+  console.log("array:", array[6]);
+});
