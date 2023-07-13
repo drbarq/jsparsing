14d83053191d634199001c75ecd9afbcdbdea96e
Joe Tustin <J.Tustin@gmail.com>
2023-07-13 15:55:18 -0600

    adding in functions to traverse array

--- serviceCatalog/csvDic/index.js
+++ serviceCatalog/csvDic/index.js
@@ -11,8 +11,7 @@ function csvToDictionary(csvFilePath, callback) {
       dictionary[key] = data;
       rowIndex++;
       for (var key2 in data) {
-        value = data[key2];
-        console.log(value, "value");
+        // value = data[key2];
         dictionary[key] = data;
       }
     })
@@ -24,15 +23,50 @@ function csvToDictionary(csvFilePath, callback) {
     });
 }
 
-// Usage:
-const csvFilePath = "./migration.csv";
+function csvToArray(csvFilePath, callback) {
+  var migrationArray = [];
+  //   let rowIndex = 1;
+  fs.createReadStream(csvFilePath)
+    .pipe(csv())
+    .on("data", (data) => {
+      //   const key = `row_${rowIndex}`; // Generate a unique identifier based on row number
 
-csvToDictionary(csvFilePath, (error, dictionary) => {
+      //   dictionary[key] = data;
+      //   rowIndex++;
+      migrationArray.push(data);
+      //   for (var key2 in data) {
+      //     // value = data[key2];
+      //     migrationArray.push(data);
+      //     // dictionary[key] = data;
+      //   }
+    })
+    .on("end", () => {
+      callback(null, migrationArray);
+    })
+    .on("error", (error) => {
+      callback(error);
+    });
+}
+
+// Usage:
+const csvFilePath = "./migration2.csv";
+
+// csvToDictionary(csvFilePath, (error, dictionary) => {
+//   if (error) {
+//     console.error("Error:", error);
+//     return;
+//   }
+
+//   console.log("Dictionary:", dictionary);
+//   console.log("Dictionary:", dictionary);
+//   //   return;
+// });
+csvToArray(csvFilePath, (error, array) => {
   if (error) {
     console.error("Error:", error);
     return;
   }
 
-  console.log("Dictionary:", dictionary);
+  console.log("array:", array[6]);
   //   return;
 });
